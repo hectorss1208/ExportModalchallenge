199c08a9cbeb65928a5385affd56aa9920bb3771
Hector Solano <hectorsolano.ucr@gmail.com>
2019-05-13 12:09:36 -0600

    commit expoModalChallenge v2

--- js/script.js
+++ js/script.js
@@ -1,42 +1,63 @@
+//run when page is executed
 window.onload = function(){
+    
+    //add evento to the button and add the funtion showModal
+    document.querySelector('.btn-export').addEventListener("click", showModal);
+    
+    //get the divs and add to variable.
+    const WRAPPER= document.querySelector('.modal-wrapper');
+    const BACKDROOP= document.querySelector('.modal-backdroop');
 
     
-    document.querySelector('.btn-export').addEventListener("click", showModal);
-    var panel= document.querySelector('.modal-wrapper');
-
     function showModal(event){
-        event.preventDefault(); 
-        panel.classList.remove("hide");
-        panel.classList.add("show");
-        this.style.opacity = "0";
+      //prevent the redirect of the page.
+        event.preventDefault();    
+        
+        //call funtion to add or remove the modal.
+        showOrHide(WRAPPER, true);
+        showOrHide(BACKDROOP, true);
+
+        //call funtion to incress the progress bar.
         move();            
     }
 
     function move() {
-        var elem = document.getElementById("myBar");
-        var exportValue = document.getElementById("exportValue");
-        
-        var width = 1;
-        var id = setInterval(frame, 100);
+        const BAR = document.getElementById("myBar");
+        const EXPORTVALUE = document.getElementById("exportValue");        
+        let width = 1;
+        const id = setInterval(frame, 100);
+
+        //function to increment he progress bar
         function frame() {
           if (width >= 100) {
-            exportValue.innerHTML=`<p>Exporting compeleted</p>`
+            //insert text to the div exportValue
+            EXPORTVALUE.innerHTML=`<p>Exporting compeleted</p>`
             clearInterval(id);
             width = 1;
-            var timerId=setInterval(function(){
-                panel.classList.remove("show");
-                panel.classList.add("hide"); 
-                document.querySelector('.btn-export').style.opacity= "1";
+            //hidde de modal
+            const timerId=setInterval(function(){                
+                showOrHide(WRAPPER, false);
+                showOrHide(BACKDROOP, false);        
                clearInterval(timerId);
               }, 3000);
           } else {
+            //increment the % in the progress bar and isert by html
             width++; 
-            elem.style.width = width + '%'; 
-            elem.innerHTML = `<span>${width}%</span>`
-            exportValue.innerHTML=`<p>Exporting...</p>`
-            
+            BAR.style.width = width + '%'; 
+            BAR.innerHTML = `<span>${width}%</span>`
+            EXPORTVALUE.innerHTML=`<p>Exporting...</p>`            
           }
         }
+      }
+
+      function showOrHide(panel, show ){
+        if(show){
+          panel.classList.remove("hide");
+          panel.classList.add("show");
+        }else{
+          panel.classList.remove("show");
+          panel.classList.add("hide");  
+        }      
       }
 
 }
